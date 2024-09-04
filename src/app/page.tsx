df696b107eee093fffebd5c04b50f100bcf7e400
Wali Ullah <waliullah0540@gmail.com>
2024-09-04 15:23:59 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>NextJS Application deployed to the AWS EC2</h1>
+      <h1 className="text-5xl text-blue-700 text-center">
+        NextJS Application deployed to the AWS EC2
+      </h1>
     </main>
   );
 }
